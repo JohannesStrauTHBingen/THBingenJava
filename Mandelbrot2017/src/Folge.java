8d3f518e6eaa920964c97864024a7ae64e1b966d
Johannes <johannes.a.strauss@th-bingen.de>
2017-06-22 15:39:07 +0200

    Neuster Stand

--- Mandelbrot2017/src/Folge.java
+++ Mandelbrot2017/src/Folge.java
@@ -1,23 +1,26 @@
 
+import javafx.scene.paint.Color;
+
 public class Folge {
 
 	private double realZ;
 	private double imgZ;
 	private double realC;
 	private double imgC;
+	private static Color[] colors = { Color.RED, Color.ORANGE, Color.YELLOW, Color.BLUE, Color.VIOLET };
 
 	public Folge() {
 	}
 
-	// Apfelmänchen: Z = 0, C =  bildpunkt
-	//Julia: Z = bildpunkt, C = beliebig
-	
-	
+	// Apfelmänchen: Z = 0, C = bildpunkt
+	// Julia: Z = bildpunkt, C = beliebig
+
 	/*
-	 * @param int maxItereation: Die Anzahl der maximal durchzuführenden Iterationen (Anzahl der verfügbaren Farben)
-	 * return int die durchgeführten iterationen
+	 * @param int maxItereation: Die Anzahl der maximal durchzuführenden
+	 * Iterationen (Anzahl der verfügbaren Farben) return int die durchgeführten
+	 * iterationen
 	 */
-	public int iterate(int maxIteration) {
+	public Color iterate() {
 		int steps = 0;
 		double border = 2;
 		double realN = realZ;
@@ -25,19 +28,22 @@ public class Folge {
 		double realNminus1 = realZ;
 		double imgNminus1 = imgZ;
 
-		for (int i = steps; i < maxIteration; i++) {
+		while (steps < colors.length) {
+
 			realN = (realNminus1 * realNminus1) - (imgNminus1 * imgNminus1);
 			imgN = (realNminus1 * imgNminus1) * 2;
 			realN += realC;
 			imgN += imgC;
 
-			if (Math.abs(Math.sqrt(realN * realN) + (imgN * imgN)) > border) {
-				return steps;
+			if (Math.abs(Math.sqrt(realN * realN) + (imgN * imgN)) >= border) {
+				return colors[steps];
 			}
 			realNminus1 = realN;
 			imgNminus1 = imgN;
+
+			steps++;
 		}
-		return steps;
+		return Color.BLACK;
 	}
 
 	public Folge setRealZ(double realC) {
--- Mandelbrot2017/src/Main.java
+++ Mandelbrot2017/src/Main.java
@@ -1,25 +1,44 @@
 
-	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+
+		
+		
+		
 		try {
+			WritableImage image = new WritableImage(1000, 1000);
+			PixelWriter pixelWriter = image.getPixelWriter();
+
+			for (int x = 0; x < image.getWidth(); x++) {
+				for (int y = 0; y < image.getHeight(); y++) {
+					double xD = x / 1000;
+					double yD = y / 1000;
+					pixelWriter.setColor(x, y, new Folge().setRealZ(0).setImgZ(0).setRealC(xD).setImgC(yD).iterate());
+				}
+			}
+
+			ImageView imageView = new ImageView(image);
+
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			root.getChildren().add(imageView);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
